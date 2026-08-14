37fc35ed28d5b567015394ab9067b2210139858f
Ovis47 <sotaro47@gmail.com>
2026-08-15 08:49:15 +0900

    Restore default comma boot logo and spinner

--- frogpilot/common/frogpilot_functions.py
+++ frogpilot/common/frogpilot_functions.py
@@ -191,12 +191,12 @@ def setup_frogpilot(build_metadata):
   THEME_SAVE_PATH.mkdir(parents=True, exist_ok=True)
 
   boot_logo_location = Path("/usr/comma/bg.jpg")
-  frogpilot_boot_logo = Path(__file__).parents[1] / "assets/other_images/frogpilot_boot_logo.png"
-  if not filecmp.cmp(frogpilot_boot_logo, boot_logo_location, shallow=False):
+  stock_boot_logo = Path(__file__).parents[1] / "assets/other_images/stock_bg.jpg"
+  if not filecmp.cmp(stock_boot_logo, boot_logo_location, shallow=False):
     stock_mount_options = subprocess.run(["findmnt", "-no", "OPTIONS", "/"], capture_output=True, text=True, check=True).stdout.strip()
 
     run_cmd(["sudo", "mount", "-o", "remount,rw", "/"], "Successfully remounted / as read-write", "Failed to remount / as read-write")
-    run_cmd(["sudo", "cp", frogpilot_boot_logo, boot_logo_location], "Successfully replaced boot logo", "Failed to replace boot logo")
+    run_cmd(["sudo", "cp", stock_boot_logo, boot_logo_location], "Successfully replaced boot logo", "Failed to replace boot logo")
     run_cmd(["sudo", "mount", "-o", f"remount,{stock_mount_options}", "/"], "Successfully restored stock mount options", "Failed to restore stock mount options")
 
   if build_metadata.channel == "FrogPilot-Development" and Path("/persist/frogsgomoo.py").is_file():
